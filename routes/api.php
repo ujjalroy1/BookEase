808521fb48e9b265453efbec9dbb23b2a2a045b9
unknown <ujjalroy1011@gmail.com>
2025-07-30 12:15:41 +0600

    all services test

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ServiceController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('services', [ServiceController::class, 'index']);
